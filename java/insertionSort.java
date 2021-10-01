c704573ab1e6591fec34379a87329a58bf304d1d
Damarla Venkata Sai Chandana <66585358+chandu6111@users.noreply.github.com>
2021-10-01 20:04:38 +0530

    Add files via upload

---
+++ java/insertionSort.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class insertionSort{
+	static void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+
+	
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array size: ");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter array elements: ");
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		sort(arr);
+		System.out.println("Sorted array: ");
+		printArray(arr);
+	}
+}
+
+/*
+Example Output:
+Enter array size: 
+5
+Enter array elements: 
+123 45 76 87 94
+Sorted array: 
+45 76 87 94 123 
+
+*/
