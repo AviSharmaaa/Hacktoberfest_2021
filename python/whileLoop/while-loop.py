b6d06a251935e9b1ece6cb30549a1988c24f34cf
ronald <ronald203006@gmail.com>
2021-10-04 11:27:03 +0700

    Hacktober

---
+++ python/whileLoop/while-loop.py
@@ -0,0 +1,8 @@
+# Initialize counter
+counter = 1
+# Iterate the loop 5 times
+while counter < 6:
+    # Print the counter value
+    print ("The current counter value: %d" % counter)
+    # Increment the counter
+    counter = counter + 1
