3f95260b15422fcd742d385cc7998e7cefcca86a
Shubh Dholakiya <53867459+shubhdholakiya@users.noreply.github.com>
2021-10-18 08:53:29 +0530

    Update Find_the_Missing_Number.cpp

--- cpp/Find_the_Missing_Number.cpp
+++ cpp/Find_the_Missing_Number.cpp
@@ -1,7 +1,6 @@
 // Problem: You are given a list of n-1 integers and these integers are in the range of 1 to n. 
 // There are no duplicates in the list.
 // One of the integers is missing in the list. 
-// Write an efficient code to find the missing integer.
 
 // I/p: arr[] = {1,2,3,5}
 // O/p: 4
